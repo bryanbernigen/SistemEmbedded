3c788bbfa569750e810416b679a6bbf76e3cd5f0
Bryan Bernigen <bryanbernigen@gmail.com>
2023-03-18 15:42:55 +0700

    Tutorial 5

--- Tutorial5/gabungan/gabungan.ino
+++ Tutorial5/gabungan/gabungan.ino
@@ -1,45 +1,63 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
 #include <Wire.h>
- 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <ESP32Servo.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+Servo myservo; 
+int pos = 0;    // variable to store the servo position
+float currentPWM = 0.0;
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+int servoPin = 13;
+
 void setup() {
-  Wire.begin();
   Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+  myservo.attach(servoPin);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
 }
- 
+
 void loop() {
-  byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-      nDevices++;
-    }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
+  for (pos = 1000; pos <=2000; pos += 100) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    currentPWM = (pos-1000)/1000.0;
+    myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
+    //clear display
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.print("Servo Current PWM: ");
+    display.setTextSize(2);
+    display.setCursor(0,10);
+    display.println(currentPWM, 3);
+    display.display(); 
+    delay(200);                       // waits 15ms for the servo to reach the position
   }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
+  for (pos = 2000; pos >= 1000; pos -= 100) { // goes from 180 degrees to 0 degrees
+    myservo.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
+    currentPWM = (pos-1000)/1000.0;
+    //clear display
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.print("Servo Current PWM: ");
+    display.setTextSize(2);
+    display.setCursor(0,10);
+    display.println(currentPWM, 3); 
+    display.display(); 
+    delay(200);                       // waits 15ms for the servo to reach the position
   }
-  else {
-    Serial.println("done\n");
-  }
-  delay(5000);          
+
+
 }
